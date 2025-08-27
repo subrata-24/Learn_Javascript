3de0b3127548a03f165609e64190af2a5eb0dd9e
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-28 00:26:22 +0600

    Make API call by function so that for every request there is no need to write the whole code again. For new call just send the method and URL.

--- XMLHttpRequest/xhr.js
+++ XMLHttpRequest/xhr.js
@@ -1,32 +1,23 @@
-// Clear the browser console before running (so only fresh logs appear)
 console.clear();
 
-// Function to fetch data from an API using XMLHttpRequest
-const getData = () => {
-    // 1. Create a new XMLHttpRequest object (used to communicate with the server)
-    const xhr = new XMLHttpRequest();
-    
-    // 2. Configure the request:
-    //    - Method: GET
-    //    - URL: Free public API that returns fake posts in JSON format
-    xhr.open('GET', "https://jsonplaceholder.typicode.com/posts");
 
-    // 3. Event handler for successful response
-    xhr.onload = () => {
-        // xhr.response contains the response data (as text by default)
-        // Parse the JSON string into a JavaScript object/array
+const makeRequest = (method, url) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {   
         let data = xhr.response;
-        console.log(JSON.parse(data)); // Log parsed data to the console
+        console.log(JSON.parse(data)); 
     }
 
-    // 4. Event handler for network errors (e.g., no internet connection)
     xhr.onerror = () => {
         console.log("Error occurred while making the request");
     }
 
-    // 5. Send the request to the server
     xhr.send();
 }
+const getData = () => {
+    makeRequest('GET', "https://jsonplaceholder.typicode.com/posts") 
+}
 
-// Call the function to start the API request
+
 getData();
