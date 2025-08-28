0c7f6514e7bee8611ba5b9bd19924ec40e58f352
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 01:51:39 +0600

    make get request by asynchronous function in fetch API calling

--- Fetch/fetchAPI.js
+++ Fetch/fetchAPI.js
@@ -3,14 +3,14 @@ console.clear();
 // fetch does not reject on HTTP errors like 404 or 500.
 // It only rejects on network errors.
 
-fetch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: "DELETE",
-})
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+const makeRequest = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return data;
+};
+
+const getData = () => {
+  makeRequest().then((res) => console.log(res));
+};
+
+getData();
