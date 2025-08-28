4138b12877875bfd761c881502cae78dc412aec5
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-28 22:31:23 +0600

    Post a mock data for learning

--- XMLHttpRequest/xhr.js
+++ XMLHttpRequest/xhr.js
@@ -1,9 +1,13 @@
 console.clear();
 
 
-const makeRequest = (method, url) => {
+const makeRequest = (method, url, data) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
+
+    //Tell server that we send JSON data
+    xhr.setRequestHeader( 'Content-type', 'application/json')
+
     xhr.onload = () => {   
         let data = xhr.response;
         console.log(JSON.parse(data)); 
@@ -13,11 +17,19 @@ const makeRequest = (method, url) => {
         console.log("Error occurred while making the request");
     }
 
-    xhr.send();
+    //make the data in JSON formate before sending
+    xhr.send(JSON.stringify(data));
 }
 const getData = () => {
     makeRequest('GET', "https://jsonplaceholder.typicode.com/posts") 
 }
 
+const sendData = () => {
+    makeRequest('POST', "https://jsonplaceholder.typicode.com/posts",{
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  })
+}
 
-getData();
+sendData();
