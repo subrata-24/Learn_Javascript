69ac59de658c208d134b6494d2d31b64b12680f9
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 01:22:58 +0600

    Handle fetch behavior: note that fetch does not reject on HTTP errors (404/500), only on network errors

--- Fetch/fetchAPI.js
+++ Fetch/fetchAPI.js
@@ -1,5 +1,8 @@
 console.clear()
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://jsonplaceholder.typicode.com/posts/101')
     .then((res) => res.json())
-.then((res)=>console.log(res))
+    .then((res) => console.log(res))
+    .catch((err) => {
+        console.log(err)
+    })
