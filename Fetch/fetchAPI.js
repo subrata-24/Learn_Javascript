f24ef70df5952e9dea3cae34ee741eb9cf58aa19
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 01:37:14 +0600

    POST method in fetch API

--- Fetch/fetchAPI.js
+++ Fetch/fetchAPI.js
@@ -3,7 +3,17 @@ console.clear()
 // fetch does not reject on HTTP errors like 404 or 500.
 // It only rejects on network errors.
 
-fetch('https://jsonplaceholder.typicode.com/posts/101')
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST",
+    body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
   .then((res) => {
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
