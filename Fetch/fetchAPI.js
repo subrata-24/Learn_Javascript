b8d926853574471439348d4dedfb92c41142ab09
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 02:02:55 +0600

    POST method: Add data in asynchronous fetch API calling

--- Fetch/fetchAPI.js
+++ Fetch/fetchAPI.js
@@ -3,14 +3,36 @@ console.clear();
 // fetch does not reject on HTTP errors like 404 or 500.
 // It only rejects on network errors.
 
-const makeRequest = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+const makeRequest = async (url, config) => {
+  const res = await fetch(url, config);
+  if (!res.ok) {
+    const message = `Error: ${res.status}`;
+    throw new Error(message);
+  }
   const data = await res.json();
   return data;
 };
 
 const getData = () => {
-  makeRequest().then((res) => console.log(res));
+  makeRequest("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 };
 
-getData();
+const sendData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+sendData();
