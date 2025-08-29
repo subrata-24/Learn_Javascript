7bdbd9af2c7262890cbf1e33081cb823a4a583b2
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 12:32:16 +0600

    DELETE: To delete some id

--- axiosAPI/axios.js
+++ axiosAPI/axios.js
@@ -1,11 +1,8 @@
 console.clear();
 
 axios
-  .patch("https://jsonplaceholder.typicode.com/posts/1", {
-    method: "PATCH",
-    body: JSON.stringify({
-      title: "fooDA",
-    }),
+  .delete("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
   })
   .then((res) => console.log(res.data))
   .catch((err) => console.log(err));
