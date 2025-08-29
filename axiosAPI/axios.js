8a97dcb12f764a9a281ecb7d423ef55d4b3da35f
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-29 12:24:39 +0600

    POST: Add some data by axios API calling

--- axiosAPI/axios.js
+++ axiosAPI/axios.js
@@ -1,6 +1,16 @@
 console.clear();
 
 axios
-  .get("https://jsonplaceholder.typicode.com/posts/101")
+  .post("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
   .then((res) => console.log(res.data))
   .catch((err) => console.log(err));
